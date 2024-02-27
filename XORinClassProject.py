92a34b008d587b1daea9ca3869fa501d407a6517
eth4npham Pham <ethanpham0918@gmail.com>
2024-02-27 19:58:52 +0000

    new

--- XORinClassProject.py
+++ XORinClassProject.py
@@ -17,3 +17,42 @@ def decode(binary):
     charIndex = int(binary, 2)
     return characters[charIndex]
 
+def XOR (bit1, bit2):
+    if bit1 == bit2:
+        return "0"
+    else:
+        return "1"
+
+def XORonByte(byte, key):
+
+    ebyte = ""
+
+    for i in range(len(byte)): 
+        ebyte += XOR(byte[i],key[i])
+
+    return ebyte
+
+def XORonLetter(letter, keyLetter):
+    binaryLetter = encode(letter)
+    binaryKeyLetter = encode (keyLetter)
+
+    encryptedLetter = XORonByte(binaryLetter,binaryKeyLetter)
+
+    return decode(encryptedLetter)
+
+def XORonSentence(sentence,key):
+
+    esentence = ""
+
+    for i in range(len(sentence)):
+        esentence += XORonLetter(sentence[i], key[i])
+
+    return esentence
+
+print(XORonSentence("hello","world"))
+print (XORonSentence("rkAan","world"))
+print(XORonSentence("&avrvLYpjgiWtmewbSfq bl", "Beaver believers, leave"))
+print(XORonSentence("aaaaaankao  )lx@EAC@?wyz", "Never lend a penguin your gown"))
+print(XORonSentence("aaaaaaaaaaaaufdInK#uaaardd!?eeejMaynC", "Never gonna frown, and roam away, adieu"))
+
+ 
